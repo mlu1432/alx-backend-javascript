a07d403b74ad944b520a9a3618f492cda5766c29
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-08-09 18:58:45 +0000

    fixing errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,12 @@
-// Function to demonstrate the use of const for variable declaration
 export function taskFirst() {
-  // Using const to declare a constant variable
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function to return a string that will be appended
 export function getLast() {
   return ' is okay';
 }
 
-// Function to demonstrate the use of let for variable declaration and string concatenation
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
